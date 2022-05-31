9acd54a512789f953bad3f67abbca8450ad7d851
Bisous31 <101545752+Bisous31@users.noreply.github.com>
2022-05-31 10:47:29 +0300

    update ModelVue file
    First update the file Model (access to fetch post data)

---
+++ Model.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <form>
+        <label> Ajouter du text </label><br>
+        <input type="text" name="name" id="name" ref="input"><br>
+        <input type="text" name="" id="">
+        <button  @click.prevent="clickbtn()">
+            envoie
+        </button>
+    </form>
+</div>    
+</template>
+<script>
+export default { 
+methods:{
+    clickbtn(){
+        const name = this.$refs.input.value
+        console.log("We are in!!!" + name);
+        fetch("http://localhost:4000/submitFormToNotion",{
+            method: "post",
+            headers:{
+                "Accept": "application/json",
+                "Content-type":"application/json"
+            },
+            body: JSON.stringify({
+                name: name
+            })
+        }).then(res => console.log(res.json()))
+        .catch((error)=>{
+            console.log(error);
+       });
+        }         
+    }
+}
+</script>
+<style>
+    
+</style>
